2decf9e5423635b0a5fafea8c73ea984bf3c19ae
umr-33 <xx_yr@icloud.com>
2019-12-09 19:11:35 +0900

    Update 20191209040334_create_events.rb

--- db/migrate/20191209040334_create_events.rb
+++ db/migrate/20191209040334_create_events.rb
@@ -4,8 +4,10 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :title
       t.datetime :start
       t.datetime :end
-      t.string :coler
+      t.string :color
       t.boolean :allday
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
