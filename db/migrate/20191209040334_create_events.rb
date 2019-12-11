8d0f3abe6fdb0fac4d2acb8fc470996c996d4ae3
umr-33 <xx_yr@icloud.com>
2019-12-11 14:06:30 +0900

    migrate修正

--- db/migrate/20191209040334_create_events.rb
+++ db/migrate/20191209040334_create_events.rb
@@ -1,5 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
+
+    create_table :group_users do |t|
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+    
     create_table :events do |t|
       t.string :title
       t.datetime :start
