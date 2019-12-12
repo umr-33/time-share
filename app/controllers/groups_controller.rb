bd85302a842328107fd2e5d2e2ced656fa3fac78
umr-33 <xx_yr@icloud.com>
2019-12-12 19:12:33 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,40 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update, :show, :destroy]
+
+  def index
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path, notice: 'グループを作成しました'
+    else
+      render :show
+    end
+  end
+
+  def show
+    @events = @group.events.includes(:user)
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_event_path(@group), notice: '編集しました'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, {user_ids: [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
