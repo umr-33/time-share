3acc687c0d36965229e15139eb38bd226c3d94ec
umr-33 <xx_yr@icloud.com>
2019-12-12 19:12:13 +0900

    eventをgroupにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'calenders#index'
-  resources :events
-  resources :users, only: [:index, :edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  root to: 'users#show'
+
+  resources :users, only: [:index, :edit, :update, :show]
+  resources :groups, only: [:new, :create, :edit, :update, :show] do
+    resources :events, only: [:new, :edit, :show, :create, :destroy]
+  end
 end
